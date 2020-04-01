8fe3ebd7efeb0d522beece375cbdc4f136016e72
“Foster <fosterataylor@yahoo.com>
2020-04-01 10:59:41 -0600

    Add MovieContainer2018 container

---
+++ src/containers/MovieContainer2018/MovieContainer2018.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import MovieCard from '../../components/MovieCard/MovieCard';
+import { useSelector } from 'react-redux';
+import './MovieContainer2018.css';
+
+
+const MovieContainer2018 = () => {
+  const { movies } = useSelector(state => state);
+  const displayTopMovies2018 = movies.filter(movie => movie.Year === '2018')
+  const movies2018 = displayTopMovies2018.map((film,index) => {
+    return (
+      <MovieCard
+      key={index}
+      id= {index}
+      Title={film.Title}
+      Poster={film.Poster} 
+      />
+    )
+  });
+  
+  return (
+    <div className='movie-container' >       
+      {movies2018}
+    </div>
+  )
+}
+
+export default MovieContainer2018
---
+++ src/containers/MovieContainer2019/MovieContainer2019.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import MovieCard from '../../components/MovieCard/MovieCard';
+import { useSelector } from 'react-redux';
+import './MovieContainer2019.css';
+
+
+const MovieContainer2019 = () => {
+  const { movies } = useSelector(state => state);
+  const displayTopMovies2019 = movies.filter(movie => movie.Year === '2019')
+  const movies2019 = displayTopMovies2019.map((film,index) => {
+    return (
+      <MovieCard
+      key={index}
+      id= {index}
+      Title={film.Title}
+      Year={film.Year}
+      Rated={film.Rated}
+      Released={film.Released}
+      Runtime={film.Runtime}
+      Genre={film.Genre}
+      Director={film.Director}
+      Writers={film.Writers}
+      Actors={film.Actors}
+      Plot={film.Plot}
+      Language={film.Language}
+      Country={film.Country}
+      Awards={film.Awards}
+      Poster={film.Poster} 
+      />
+    )
+  });
+  
+  return (
+    <div className='movie-container' >       
+      {movies2019}
+    </div>
+  )
+}
+
+export default MovieContainer2019
