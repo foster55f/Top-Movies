c2c0e2a92989c30b938483a27f856c567ac612a4
“Foster <fosterataylor@yahoo.com>
2020-03-31 19:39:48 -0600

    Add MovieContainer component

---
+++ MovieContainer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import MovieCard from '../MovieCard/MovieCard';
+
+export const MovieContainer = ({ movies, user}) => {
+
+  return (
+    <div className='movie-container'>
+      {movies.map(film => {
+        return (
+          <MovieCard
+            key={film.id}
+            id={film.id}
+            title={film.title}
+            poster={film.poster_path}
+            averageRating={film.average_rating}
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+export const mapStateToProps = state => ({
+  movies: state.movies
+})
+
+export default connect(mapStateToProps)(MovieContainer)
