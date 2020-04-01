2d1f65228e26c018f1c3a3f90e379175959f209f
“Foster <fosterataylor@yahoo.com>
2020-03-31 20:15:45 -0600

    Add initial styling and display posters for movies

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { retrieveMovies } from './fetchcalls'
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { addMovies } from './actioncreators';
+import './App.css';
 import { connect } from 'react-redux'
+import MovieContainer from './MovieContainer';
 
 const App = () => {
   const dispatch = useDispatch()
@@ -20,8 +22,9 @@ const App = () => {
   }
 
   return (
-    <div>
+    <div className='App'>
       <button onClick={handleClick}>You clicked times</button>
+      <MovieContainer />
     </div>
   );
 }
