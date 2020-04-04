f124b77961ad07ebb0d4d5198c5fd5dd456b4691
“Foster <fosterataylor@yahoo.com>
2020-04-04 16:55:29 -0600

    Add escape button functionality to MovieShowPage

--- src/components/MovieShowPage/MovieShowPage.js
+++ src/components/MovieShowPage/MovieShowPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addFavoriteMovies, removeFavoriteMovies } from '../../actioncreators';
 import EscapeListen from "../../components/EscapeListen/EscapeListen";
@@ -7,12 +7,28 @@ import { withRouter } from 'react-router-dom';
 
 
 
-const MovieShowPage = (props) => {
+const MovieShowPage = (props, history) => {
   const { movies } = useSelector(state => state);
+  const [currentKey, setCurrentKey] = useState(null);
   const { favoriteMovies} = useSelector(state => state);
   const dispatch = useDispatch();
   const isAFavorite = favoriteMovies.find(film => film.imdbID === props.movie.imdbID);
   const [hasBeenFavorited, setFavorite] = useState(favoriteMovies.includes(isAFavorite));
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      console.log(event)
+      setCurrentKey(event.keyCode);
+      if(event.keyCode === 27) {
+        props.history.push("/")
+      }
+    }
+    document.addEventListener('keydown', handleKeyPress);
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  });
 
 
   const pickFavoriteMovies = (id) => {
@@ -29,12 +45,6 @@ const MovieShowPage = (props) => {
         return film.Title === id
     })
     dispatch(removeFavoriteMovies(favorite))
-  }
-
-  const keyPressed =(event)=> {
-    if (event.key === "Enter") {
-      console.log('hello')
-    }
   }
   
 
@@ -53,12 +63,15 @@ const MovieShowPage = (props) => {
           {!hasBeenFavorited &&
             <button id={props.movie.Title}
               onClick={event => pickFavoriteMovies(event.target.id)}
-              onKeyPress={keyPressed}
+              onKeyPress={event => this.handleKeyPress(event)}
             >Add Favorite</button>
           }
           {hasBeenFavorited &&
             <button id={props.movie.Title}
-              onClick={event => removeFavorite(event.target.id)}>Remove Favorite</button>
+              onClick={event => removeFavorite(event.target.id)}
+              onKeyPress={event => this.handleKeyPress(event)}
+            >Remove Favorite</button>
+              
             }
             </div>
             <h3>Plot</h3>
@@ -76,7 +89,6 @@ const MovieShowPage = (props) => {
             </ul>
             </div>
         </div> 
-      <EscapeListen />
       </section>
     )
 }
