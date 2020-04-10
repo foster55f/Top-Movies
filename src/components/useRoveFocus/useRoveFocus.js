ec625ad234ef4678202c69c40a835f9f463f899d
“Foster <fosterataylor@yahoo.com>
2020-04-10 11:34:17 -0600

    Add easier to read variables for arrow key codes

--- src/components/useRoveFocus/useRoveFocus.js
+++ src/components/useRoveFocus/useRoveFocus.js
@@ -13,28 +13,32 @@ const { favoriteMovies } = useSelector(state => state);
 
   const handleKeyDown = useCallback(
     e => {
-      if (e.keyCode === 39) {
+      let rightArrow = 39;
+      let lefttArrow = 37;
+      let downArrow = 40;
+      let upArrow = 38;
+      if (e.keyCode === rightArrow) {
         e.preventDefault();
         if (currentFocus.leftRight === size - 1) {
           setCurrentFocus({...currentFocus, leftRight:0})
         } else {
           setCurrentFocus({...currentFocus, leftRight: currentFocus.leftRight+1})
         }
-      } else if (e.keyCode === 37) {
+      } else if (e.keyCode === lefttArrow) {
         e.preventDefault();
         if (currentFocus.leftRight === 0 )  {
           setCurrentFocus({...currentFocus, leftRight:size-1})
         } else {
           setCurrentFocus({...currentFocus, leftRight: currentFocus.leftRight-1})
         }
-      } else if (e.keyCode === 40) {
+      } else if (e.keyCode === downArrow) {
         e.preventDefault();
         if(favoriteMovies.length > 0 && currentFocus.upDown===6 || favoriteMovies.length === 0 && currentFocus.upDown === 5 ) {
           setCurrentFocus({leftRight:0, upDown:0},)
         } else {
           setCurrentFocus({leftRight:0, upDown: currentFocus.upDown+1})
         }
-      }else if (e.keyCode === 38) {
+      }else if (e.keyCode === upArrow) {
         e.preventDefault();
         if (currentFocus.upDown === 0 && favoriteMovies.length === 0) {
           setCurrentFocus({leftRight: 0, upDown: 5})
