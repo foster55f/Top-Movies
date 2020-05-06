ae2b682b9fb7cc07fd2c572287b639a769123e8d
“Foster <fosterataylor@yahoo.com>
2020-05-06 10:55:16 -0600

    Add better structure for selenium testing

--- test/App.test.js
+++ test/App.test.js
@@ -1,36 +1,19 @@
 var assert = require('chai').assert;
 webdriver = require('selenium-webdriver');
 
-describe( 'Test Suite' , () => {
-
-    before(async()=>{
-
+it( 'Test Suite' , async() => {
     const driver = new webdriver.Builder().forBrowser('chrome').build();
         
     await driver.get('http://www.google.com');
     await driver.findElement(webdriver.By.name('q')).sendKeys('hello world');
     await driver.findElement({ name: 'q' }).sendKeys(webdriver.Key.ENTER);
-
-        
-    return driver.findElement({ xpath: '//*[@id="rso"]/div[1]/div[1]/div[1]/div[1]/div/div[1]/div[2]/div/div/div/div[2]/h3/a' }).click();
-    });
-
-    after(function () {
-    const driver = new webdriver.Builder().forBrowser('chrome').build();
-        
+ 
+    await driver.findElement({ xpath: '//*[@id="rso"]/div[2]/div/div[1]/a/h3' }).click();
+        var title = await driver.getTitle()
+        it( 'Test Case' , async()=>{
+            assert.equal(title, '"Hello, World!" program - Wikipedia');
+        })      
         driver.quit();
     });
 
-    it( 'Test Case' , async()=>{
-        const driver = new webdriver.Builder().forBrowser('chrome').build();
-        // const title = await driver.getTitle().then(function (title) { title});
-        // assert.equal(title, 'foster');
-            var title = await driver.getTitle()
-            assert.equal(title, "Google");
-        
-        // driver.sleep();
-    })
-});
-
-
-
+    
