0b60fade00de095cca0c2fcb01f2d5add9bb04b4
“Foster <fosterataylor@yahoo.com>
2020-05-06 12:36:10 -0600

    Modify 2 tests for react app

--- test/App.test.js
+++ test/App.test.js
@@ -1,19 +1,28 @@
 var assert = require('chai').assert;
 webdriver = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 
 it( 'Test Suite' , async() => {
     const driver = new webdriver.Builder().forBrowser('chrome').build();
         
-    await driver.get('http://www.google.com');
-    await driver.findElement(webdriver.By.name('q')).sendKeys('hello world');
-    await driver.findElement({ name: 'q' }).sendKeys(webdriver.Key.ENTER);
+    await driver.get('http://localhost:3000/');
+    var title = await driver.getTitle()
+            assert.equal(title, 'React App');
+
+    // await driver.findElement(webdriver.By.name('q')).sendKeys('hello world');
+    // await driver.findElement({ name: 'q' }).sendKeys(webdriver.Key.ENTER);
  
-    await driver.findElement({ xpath: '//*[@id="rso"]/div[2]/div/div[1]/a/h3' }).click();
-        var title = await driver.getTitle()
-        it( 'Test Case' , async()=>{
-            assert.equal(title, '"Hello, World!" program - Wikipedia');
-        })      
-        driver.quit();
-    });
+    // await driver.findElement({ xpath: '//*[@id="rso"]/div[2]/div/div[1]/a/h3' }).click();
+    //     var title = await driver.getTitle()
+    //         assert.equal(title, '"Hello, World!" program - Wikipedia');
+});
+
+it( 'Test Suite' , async() => {
+    const driver = new webdriver.Builder().forBrowser('chrome').build();
+        
+    await driver.get('http://localhost:3000/');
+    const header  = await driver.findElement(By.className('header')).getText();
+    assert.equal(header, 'Top Movies 2019');
+});
 
     
