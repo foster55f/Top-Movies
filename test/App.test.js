6df7db13e37404de15b5964a23e3db3018da7446
“Foster <fosterataylor@yahoo.com>
2020-05-06 13:00:01 -0600

    Add 2 selenium tests for react app with both passing

--- test/App.test.js
+++ test/App.test.js
@@ -2,22 +2,16 @@ var assert = require('chai').assert;
 webdriver = require('selenium-webdriver');
 const { Builder, By, Key, until } = require('selenium-webdriver');
 
-it( 'Test Suite' , async() => {
+it( 'Test location' , async() => {
     const driver = new webdriver.Builder().forBrowser('chrome').build();
         
     await driver.get('http://localhost:3000/');
-    var title = await driver.getTitle()
+    const title = await driver.getTitle()
             assert.equal(title, 'React App');
 
-    // await driver.findElement(webdriver.By.name('q')).sendKeys('hello world');
-    // await driver.findElement({ name: 'q' }).sendKeys(webdriver.Key.ENTER);
- 
-    // await driver.findElement({ xpath: '//*[@id="rso"]/div[2]/div/div[1]/a/h3' }).click();
-    //     var title = await driver.getTitle()
-    //         assert.equal(title, '"Hello, World!" program - Wikipedia');
 });
 
-it( 'Test Suite' , async() => {
+it( 'Test header' , async() => {
     const driver = new webdriver.Builder().forBrowser('chrome').build();
         
     await driver.get('http://localhost:3000/');
@@ -25,4 +19,3 @@ it( 'Test Suite' , async() => {
     assert.equal(header, 'Top Movies 2019');
 });
 
-    
