9b104a23b7caa1e3903cb991dbf743d425feb153
“Foster <fosterataylor@yahoo.com>
2020-05-07 17:58:17 -0600

    Refactor

--- test/App.test.js
+++ test/App.test.js
@@ -1,10 +1,9 @@
 var assert = require('chai').assert;
 webdriver = require('selenium-webdriver');
-const { Builder, By, Key, until } = require('selenium-webdriver');
+const {By} = require('selenium-webdriver');
 
 it( 'Test location' , async() => {
     const driver = new webdriver.Builder().forBrowser('chrome').build();
-        
     await driver.get('http://localhost:3000/');
     const title = await driver.getTitle()
             assert.equal(title, 'React App');
@@ -12,8 +11,7 @@ it( 'Test location' , async() => {
 });
 
 it( 'Test header' , async() => {
-    const driver = new webdriver.Builder().forBrowser('chrome').build();
-        
+    const driver = new webdriver.Builder().forBrowser('chrome').build(); 
     await driver.get('http://localhost:3000/');
     const header  = await driver.findElement(By.className('header')).getText();
     assert.equal(header, 'Top Movies 2019');
